69466a332c7d537b825c5e8d5b95ca15b62a51fd
Vikram Pasumarti <vpasuma@purdue.edu>
2016-10-17 01:16:14 -0400

    Update TerminalClient.java

--- TerminalClient.java
+++ TerminalClient.java
@@ -4,11 +4,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Numerical Data Summary Terminal - TerminalClient
- * <p>
+ *
  * This program does two tasks. The first is file reading,
  * that is, it reads the inputted text file and determines
  * if the content of the file can be used or if there is a
@@ -18,31 +19,40 @@ import java.util.Scanner;
  * user to enter commands and then executes the users input.
  *
  * @author Vikram Pasumarti, vpasuma@purdue.edu
- * @version 10 September 2016
+ * @version 17 October 2016
  */
 public class TerminalClient {
-    ArrayList<Double> dataset = new ArrayList<>();
-    double dataArray[];
-    String fileName;
-    boolean fileError = false;
 
-    public TerminalClient(String fileName) {
+    private double dataArray[];
+    private boolean fileError = false;
+    private boolean fileExists = false;
+
+    /**
+     * In the constructor, inputted files are read and their contents
+     * are written to an Array List. They are then sorted and then
+     * inputted into an array. After storing, the fileExists boolean
+     * is set to true. This is to tell the fileCheck method that there
+     * is a file loaded.
+     */
+    private TerminalClient(String fileName) {
+        ArrayList<Double> dataset = new ArrayList<>();
         File f1 = new File(fileName);
-        this.fileName = fileName;
         try {
             FileReader fr = new FileReader(f1);
             Scanner s = new Scanner(fr);
             while (s.hasNext()) {
                 if (s.hasNextDouble()) {
-                    this.dataset.add(s.nextDouble());
+                    dataset.add(s.nextDouble());
                 } else {
                     s.next();
                 }
             }
-            this.dataArray = new double[this.dataset.size()];
-            for (int i = 0; i < this.dataset.size(); i++) {
-                this.dataArray[i] = this.dataset.get(i);
+            dataArray = new double[dataset.size()];
+            for (int i = 0; i < dataset.size(); i++) {
+                this.dataArray[i] = dataset.get(i);
             }
+            Arrays.sort(dataArray);
+            fileExists = true;
 
 
         } catch (Exception e) {
@@ -52,12 +62,22 @@ public class TerminalClient {
 
     }
 
-    public String fileCheck(String fileLocation, double array[]) {
-        if (fileLocation == null) {
+    /**
+     * The fileExists boolean and the dataset array are inputted
+     * into the method. The fileExists boolean determines if a file
+     * exists, not if the file is valid. A file is considered valid if
+     * there exists numbers in it. If there are no numbers in the file,
+     * then nothing will be written to the array, which is why an array
+     * with a length of zero would mean the file is invalid. A valid
+     * file would return the string "Good file", while an invalid
+     * file returns "Bad file".
+     */
+    private String fileCheck(boolean fileExists,double array[]) {
+        if (!fileExists) {
             System.out.println("\nNo file loaded\n");
             return "Bad file";
         } else if (array.length == 0) {
-            System.out.println("File invalid. Please check " + fileLocation + " for errors or load new file");
+            System.out.println("File invalid. Please check file for errors or load new file");
             return "Bad file";
         } else {
             return "Good file";
@@ -66,13 +86,12 @@ public class TerminalClient {
     }
 
     public static void main(String[] args) {
-        String check = "";
-        String fileLocation = null;
-        boolean repeat = true;
-        String fileName = null;
+        String check;
+        String fileLocation = "";
+        String fileName = "";
         DataFunctions d = new DataFunctions();
         Scanner s = new Scanner(System.in);
-        TerminalClient tc = new TerminalClient("null.txt");
+
         System.out.println("\n" +
                 "\n" +
                 "      _   _                                     _                  _     ____            _           \n" +
@@ -88,32 +107,53 @@ public class TerminalClient {
                 "                                                             |___/                                                             \n" +
                 "\n");
         System.out.println("@author Vikram Pasumarti");
-        System.out.println("@version 10 September 2016");
+        System.out.println("@version 17 October 2016");
         System.out.println("---------------------------");
-        System.out.println("Please Enter a Command. If you need help, type 'help'");
+        System.out.println("Please select a file, then enter a command. If you need help, type 'help'");
+
+        //Initial file choosing
+        int backSlashIndex = 0;
+        JFileChooser fc1 = new JFileChooser();
+        int fileChoose1 = fc1.showOpenDialog(new JFrame());
+        if (fileChoose1 == JFileChooser.APPROVE_OPTION) {
+            fileLocation = fc1.getSelectedFile().getAbsolutePath();
+            //Determines name of file from file path
+            backSlashIndex = 0;
+            for (int i = 0; i < fileLocation.length(); i++) {
+                if (fileLocation.charAt(i) == '\\') {
+                    backSlashIndex = i;
+                }
+            }
+            fileName = fileLocation.substring(backSlashIndex + 1);
+            System.out.println("\n" + fileName + " selected" + "\n");
+        } else if (fileChoose1 == JFileChooser.CANCEL_OPTION) {
+            System.out.println("\nNo new file selected\n");
+        }
+        TerminalClient tc = new TerminalClient(fileLocation);
+        //Initiates program loop
         do {
             String command = s.next();
             switch (command) {
 
                 case "file":
-                    //System.out.println("\nEnter file name\n");
-                    JFileChooser jfc = new JFileChooser();
-                    int fileChoose = jfc.showOpenDialog(new JFrame());
-                    if (fileChoose == JFileChooser.APPROVE_OPTION) {
-                        fileLocation = jfc.getSelectedFile().getAbsolutePath();
-                    } else if (fileChoose == JFileChooser.CANCEL_OPTION) {
+                    JFileChooser fc2 = new JFileChooser();
+                    int fileChoose2 = fc2.showOpenDialog(new JFrame());
+                    if (fileChoose2 == JFileChooser.APPROVE_OPTION) {
+                        fileLocation = fc2.getSelectedFile().getAbsolutePath();
+                    } else if (fileChoose2 == JFileChooser.CANCEL_OPTION) {
                         System.out.println("\nNo new file selected\n");
                         break;
                     }
+                    //Creates new instance for new file
                     tc = new TerminalClient(fileLocation);
                     if (tc.fileError) {
                         break;
                     }
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
-                    int backSlashIndex = 0;
+                    backSlashIndex = 0;
                     for (int i = 0; i < fileLocation.length(); i++) {
                         if (fileLocation.charAt(i) == '\\') {
                             backSlashIndex = i;
@@ -123,127 +163,131 @@ public class TerminalClient {
                     System.out.println("\n" + fileName + " selected" + "\n");
                     break;
                 case "mean":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nMean of dataset " + fileName + " = " + d.mean(tc.dataArray) + "\n");
                     break;
                 case "median":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nMedian of dataset " + fileName + " = " + d.median(tc.dataArray) + "\n");
                     break;
                 case "mode":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
+                    } else if (d.mode(tc.dataArray).isEmpty()) {
+                        System.out.println("\nMode of dataset " + fileName + " = No mode" + "\n");
+                    } else {
+                        System.out.println("\nMode of dataset " + fileName + " = " + d.mode(tc.dataArray) + "\n");
                     }
-                    System.out.println("\nMode of dataset " + fileName + " = " + d.mode(tc.dataArray) + "\n");
                     break;
                 case "skew":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
-                    double medianVal = Double.parseDouble(d.median(tc.dataArray));
-                    double meanVal = Double.parseDouble(d.mean(tc.dataArray));
+                    double medianVal = d.median(tc.dataArray);
+                    double meanVal = d.mean(tc.dataArray);
 
                     System.out.println("\n Skew of dataset " + fileName + " is " + d.skew(meanVal, medianVal) + "\n");
                     break;
                 case "stddev":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nStandard Deviation of dataset " + fileName + " = " + d.stdDev(tc.dataArray) + "\n");
                     break;
                 case "range":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nRange of dataset " + fileName + " = " + d.range(tc.dataArray) + "\n");
                     break;
                 case "Q1":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nFirst Quartile of dataset " + fileName + " = " + d.Q1(tc.dataArray) + "\n");
                     break;
                 case "Q3":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nThird Quartile  of dataset " + fileName + " = " + d.Q3(tc.dataArray) + "\n");
                     break;
                 case "iqr":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nInterquartile Range of dataset " + fileName + " = " + d.iqr(tc.dataArray) + "\n");
                     break;
                 case "fivenumsum":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("Five Number Summary for " + fileName + " " + d.fiveNumSummary(tc.dataArray) + "\n");
                     break;
                 case "outliers":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("\nBad file")) {
                         break;
                     }
                     System.out.println("\nOutliers for " + fileName + " " + d.outliers(tc.dataArray) + "\n");
                     break;
                 case "listoutliers":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nList of Outliers for " + fileName + " " + d.listOutliers(tc.dataArray) + "\n");
                     break;
                 case "listdataset":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nDataset " + fileName + ": " + d.listDataset(tc.dataArray) + "\n");
                     break;
                 case "max":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nMaximum of dataset " + fileName + " = " + d.max(tc.dataArray) + "\n");
                     break;
                 case "min":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nMinimum of dataset " + fileName + " = " + d.min(tc.dataArray) + "\n");
                     break;
                 case "sort":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
                     System.out.println("\nSorted Dataset " + fileName + ": " + d.datasetSort(tc.dataArray) + "\n");
                     break;
                 case "print":
-                    check = tc.fileCheck(fileLocation, tc.dataArray);
+                    //Builds a text file containing a full summary of the data
+                    check = tc.fileCheck(tc.fileExists, tc.dataArray);
                     if (check.equals("Bad file")) {
                         break;
                     }
-                    medianVal = Double.parseDouble(d.median(tc.dataArray));
-                    meanVal = Double.parseDouble(d.mean(tc.dataArray));
+                    medianVal = d.median(tc.dataArray);
+                    meanVal = d.mean(tc.dataArray);
                     File f2 = new File(fileName + "_numerical_summary.txt");
                     try {
                         FileWriter fw = new FileWriter(f2);
@@ -295,10 +339,11 @@ public class TerminalClient {
                         pw.write("Five Number Summary for " + fileName + ": " + d.fiveNumSummary(tc.dataArray));
                         pw.println();
                         pw.println();
-                        pw.write("Outliers for " + fileName + ": \n " + d.outliers(tc.dataArray));
+                        pw.write("Outlier Conditions for " + fileName + ": \n " + d.outliers(tc.dataArray));
                         pw.println();
                         pw.println();
                         pw.write("List of Outliers for " + fileName + " \n" + d.listOutliers(tc.dataArray));
+                        fw.close();
                         pw.close();
                         System.out.println("\nText file made successfully. It will be visible once this" +
                                 " program is exited\n");
@@ -333,7 +378,6 @@ public class TerminalClient {
                     break;
                 case "exit":
                     System.out.println("\nGoodbye \n");
-                    repeat = false;
                     System.exit(0);
                     break;
                 default:
@@ -341,10 +385,11 @@ public class TerminalClient {
                     break;
             }
         }
-        while (repeat);
-
-
+        while (true);
     }
 
+
 }
+
+
 
